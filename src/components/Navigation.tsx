7202b50d7ebc79bdc0a5273e37434674c9b72cb6
hyunjin <alfma123@naver.com>
2022-06-06 21:02:42 +0900

    chore: tsx로 변환조건 추가/불필요한 구문 삭제

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './assets/logo.png';
+
+function Navigation() {
+  return (
+    <nav>
+      <img src={logo} alt='logo' />
+      <Link to='/Login'>로그인</Link>
+    </nav>
+  );
+}
+
+export default Navigation;
