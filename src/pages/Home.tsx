3095da185ae94442dacc5589680bc788919ef583
hyunjin <alfma123@naver.com>
2022-06-10 17:44:20 +0900

    fix: file형식, 위치 수정

---
+++ src/pages/Home.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import CampCard from '../components/CampCard';
+import Navigation from '../components/Navigation';
+import Visual from '../components/Visual';
+
+function Home() {
+  const [camps, setCamps] = useState([]);
+  useEffect(() => {
+    axios.get('../apis /camp.json').then((res) => {
+      setCamps(res.data);
+    });
+  }, []);
+  return (
+    <>
+      <div>
+        <Navigation />
+        <Visual />
+      </div>
+      <div>
+        <CampCard camps={camps} />
+      </div>
+    </>
+  );
+}
+
+export default Home;
